d762202ca45a3d03fa8af53a5a65ca59fb61ff4b
yes3421 <karl_heinrich1883@outlook.com>
2023-05-14 17:49:59 +0000

    Initial commit

--- Board.cpp
+++ Board.cpp
@@ -27,3 +27,21 @@ bool Board::horizontal_check() const
     }
     return false;
 }
+
+bool Board::vertical_check() const
+{
+    for (int i = 0; i < 3; ++i) {
+        if (
+            (!_board[i].is_empty())
+            || (!_board[i + 3].is_empty())
+            || (!_board[i + 6].is_empty())
+        ) {
+            return (
+                _board[i].symbol()
+                == _board[i + 3].symbol()
+                == _board[i + 6].symbol()
+            );
+        }
+    }
+    return false;
+}
