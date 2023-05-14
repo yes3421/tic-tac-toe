f6525477574b9874d63bef9de055ea5f76f6fc34
yes3421 <karl_heinrich1883@outlook.com>
2023-05-14 18:23:30 +0000

    Initial commit

--- Board.cpp
+++ Board.cpp
@@ -12,36 +12,70 @@ void Board::print() const
 
 bool Board::horizontal_check() const
 {
+    bool flag = false;
+
     for (int i = 0; i < 9; i += 3) {
         if (
             (!_board[i].is_empty())
             || (!_board[i + 1].is_empty())
             || (!_board[i + 2].is_empty())
         ) {
-            return (
+            flag = (
                 _board[i].symbol()
                 == _board[i + 1].symbol()
                 == _board[i + 2].symbol()
             );
         }
     }
-    return false;
+    return flag;
 }
 
 bool Board::vertical_check() const
 {
+    bool flag = false;
+
     for (int i = 0; i < 3; ++i) {
         if (
             (!_board[i].is_empty())
             || (!_board[i + 3].is_empty())
             || (!_board[i + 6].is_empty())
         ) {
-            return (
+            flag = (
                 _board[i].symbol()
                 == _board[i + 3].symbol()
                 == _board[i + 6].symbol()
             );
         }
     }
-    return false;
+    return flag;
+}
+
+bool Board::diagonal_check() const
+{
+    bool flag = false;
+
+    if (
+        (!_board[0].is_empty())
+        || (!_board[4].is_empty())
+        || (!_board[8].is_empty())
+    ) {
+        flag = (
+            _board[0].symbol()
+            == _board[4].symbol()
+            == _board[8].symbol()
+        );
+    }
+
+    if (
+        (!_board[2].is_empty())
+        || (!_board[4].is_empty())
+        || (!_board[6].is_empty())
+    ) {
+        flag = (
+            _board[2].symbol()
+            == _board[4].symbol()
+            == _board[6].symbol()
+        );
+    }
+    return flag;
 }
