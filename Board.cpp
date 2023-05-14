52e13816e7db7f4e358fae8d4a234907ce9f1581
yes3421 <karl_heinrich1883@outlook.com>
2023-05-14 17:34:47 +0000

    Initial commit

--- Board.cpp
+++ Board.cpp
@@ -14,14 +14,14 @@ bool Board::horizontal_check() const
 {
     for (int i = 0; i < 9; i += 3) {
         if (
-            (!_board[i].is_empty()) ||
-            (!_board[i + 1].is_empty()) ||
-            (!_board[i + 2].is_empty())
+            (!_board[i].is_empty())
+            || (!_board[i + 1].is_empty())
+            || (!_board[i + 2].is_empty())
         ) {
             return (
-                _board[i].symbol() ==
-                _board[i + 1].symbol() ==
-                _board[i + 2].symbol()
+                _board[i].symbol()
+                == _board[i + 1].symbol()
+                == _board[i + 2].symbol()
             );
         }
     }
